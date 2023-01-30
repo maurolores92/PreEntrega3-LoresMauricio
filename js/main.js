ff2995d1da631382f2aba327e0cd8af0df8142da
Mauricio <maurolores1992@gmail.com>
2023-01-30 02:20:44 -0300

    agregando slider

--- js/main.js
+++ js/main.js
@@ -340,4 +340,22 @@ function actualizarNumerito() {
     numerito.innerText = nuevoNumerito;
 }
 
-// 1.41
+// SLIDER
+
+let slideIndex = 0;
+
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if(slideIndex > slides.length) {
+        slideIndex = 1
+    }
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides,6000);
+}
