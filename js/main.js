d6429bbdf5cd833abe996bdbda229bf49fe75386
Mauricio <maurolores1992@gmail.com>
2023-01-31 00:47:27 -0300

    modificando queries mobile

--- js/main.js
+++ js/main.js
@@ -270,7 +270,7 @@ function cargarProductos(productosElegidos) {
         div.classList.add("producto");
         div.innerHTML = `
             <img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
-            <div class="producto-detalles">
+            <div class="producto-detalles"> 
                 <h3 class="producto-titulo">${producto.titulo}</h3>
                 <p class="producto-precio">$${producto.precio}</p>
                 <button class="producto-agregar" id="${producto.id}">Agregar</button>
